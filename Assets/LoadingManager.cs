3748bb2c4b218b62450a678a3e5d53b936542fa2
Cookie <88719371+Cookiezikse@users.noreply.github.com>
2024-09-13 14:13:24 +0200

    [ADD] Button Menu Hover

--- Assets/LoadingManager.cs
+++ Assets/LoadingManager.cs
@@ -6,6 +6,7 @@ using DG.Tweening;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine.UI;
+using UnityEngine.InputSystem;
 public class LoadingManager : MonoBehaviour
 {
 
@@ -49,7 +50,7 @@ public class LoadingManager : MonoBehaviour
         _quitButtonBack = GameObject.Find("QuitBack");
 
         _listButtons.Add(_playButton);
-        _listButtons.Add( _playModButton);
+        _listButtons.Add(_playModButton);
         _listButtons.Add(_optionButton);
         _listButtons.Add(_quitButton);
 
@@ -67,11 +68,11 @@ public class LoadingManager : MonoBehaviour
     {
         float duration = 0.3f;
 
-        
+
         gameObject.transform.DOScale(new Vector2(1.0f, 1.0f), duration);
         gameObject.transform.DORotate(new Vector3(0, 0, 0), duration);
-        gameObject.transform.DOMove(new Vector2(_center.transform.position.x/3f , _center.transform.position.y - (i * 60) - moreSize), duration).SetEase(Ease.OutCubic);
-    
+        gameObject.transform.DOMove(new Vector2(_center.transform.position.x / 3f, _center.transform.position.y - (i * 120) - moreSize), duration).SetEase(Ease.OutCubic);
+
     }
 
 
@@ -89,8 +90,8 @@ public class LoadingManager : MonoBehaviour
         gameObject.transform.DOScale(new Vector2(1.2f, 1.2f), duration);
         gameObject.transform.DORotate(new Vector3(0, 0, 0), duration);
         gameObject.transform.DOMove(new Vector2(_center.transform.position.x, _center.transform.position.y - (i * 60) - moreSize), duration).SetEase(Ease.OutCubic);
-    
-        
+
+
     }
 
     int i = 1;
@@ -103,24 +104,24 @@ public class LoadingManager : MonoBehaviour
 
             Transform currentCube = cube;
 
-            currentCube.DORotate(new Vector3(0, 0, 360), 0.8f,RotateMode.FastBeyond360).OnComplete(() =>
+            currentCube.DORotate(new Vector3(0, 0, 360), 0.8f, RotateMode.FastBeyond360).OnComplete(() =>
                 {
                     if (currentCube != gameObject.transform)
-                    { 
+                    {
                         if (currentIndex % 2 == 0)
                         {
-                            currentCube.DOScale(new Vector2(1.5f,1.5f), 3f).SetLoops(-1, LoopType.Yoyo);
+                            currentCube.DOScale(new Vector2(1.5f, 1.5f), 3f).SetLoops(-1, LoopType.Yoyo);
                             currentCube.DORotate(new Vector3(0, 0, -360), 3.5f, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Yoyo);
                         }
                         else
                         {
-                            currentCube.DORotate(new Vector3(0, 0, -360), 2.5f, RotateMode.FastBeyond360).SetLoops(-1,LoopType.Yoyo);
+                            currentCube.DORotate(new Vector3(0, 0, -360), 2.5f, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Yoyo);
                         }
                     }
                 });
-            
+
             i++;
-            
+
         }
     }
 
@@ -128,132 +129,161 @@ public class LoadingManager : MonoBehaviour
 
     private void Update()
     {
-        
-        
-        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            ChangeSelectedButton(+1);
-            ChangeColors();
-            ChangePositionButton();
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            ChangeSelectedButton(-1);
-            ChangeColors();
-            ChangePositionButton();
-        }
 
-        if (Input.GetKeyDown(KeyCode.KeypadEnter))
-        {
-            DoActionButton();
-        }
 
-    }
-
-    void ChangePositionButton()
-    {
         for (int i = 0; i < _listButtons.Count; i++)
         {
-            if (i == _selectedButton)
+
+            Vector2 buttonPosition = new Vector2(_listButtons[i].transform.position.x, _listButtons[i].transform.position.y);
+
+            Vector3 mousePosition = Input.mousePosition;
+
+            Vector2 mousePosition2D = new Vector2(mousePosition.x, mousePosition.y);
+
+            float size = 50;
+
+            //Debug.Log("MOUSE POSITION " +mousePosition2D.ToString());
+            //Debug.Log("BUTTON POSITION " + buttonPosition.ToString());
+
+            if (buttonPosition.x - size  < mousePosition2D.x &&
+                buttonPosition.x + size > mousePosition2D.x &&
+                buttonPosition.y - size < mousePosition2D.y &&
+                buttonPosition.y + size > mousePosition2D.y)
             {
-                AnimationGoCenter(_listButtons[i], i);
-                AnimationGoCenter(_listButtonsBackgrounds[i], i, 40);
-                AnimationCenterCubes(_listButtonsBackgrounds[i]);
+                _selectedButton = i;
+                ChangeColors();
+                ChangePositionButton();
+
+                break;
             }
-            else
+
+        }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.RightArrow))
             {
-                AnimationGoLeft(_listButtons[i], i);
-                AnimationGoLeft(_listButtonsBackgrounds[i], i, 20);
+                ChangeSelectedButton(+1);
+                ChangeColors();
+                ChangePositionButton();
             }
-        }
-    }
-
-    void DoActionButton()
-    {
-        switch(_selectedButton)
-        {
-            case 0:
-                Play();
-                break;
-            case 1:
-                PlayEaster();
-                break;
-            case 2:
-                Options();
-                break;
-            case 3:
-                Quit();
-                break;
-        }
-    }
-
-    void Play()
-    {
-        _playButton.GetComponent<TextMeshProUGUI>().DOColor(Color.black, 0.2f);
-        SceneManager.LoadScene("MainScene");
-    }
-
-    void PlayEaster()
-    {
-        _playModButton.GetComponent<TextMeshProUGUI>().DOColor(Color.black, 0.2f);
-        SceneManager.LoadScene("DBDMode");
-
-    }
-
-    void Options()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void Quit()
-    {
-        Application.Quit();
-    }
-
-    void ChangeSelectedButton(int i)
-    {
-        _selectedButton += i;
-        if (_selectedButton < 0)
-        {
-            _selectedButton = _listButtons.Count - 1;
-        }
-        else if (_selectedButton >= _listButtons.Count)
-        {
-            _selectedButton = 0;
-        }
-    }
-
-    void ChangeColors()
-    {
-        for (int i = 0; i < _listButtons.Count; i++)
-        {
-            if (i == _selectedButton)
+            if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.LeftArrow))
             {
-                _listButtons[i].GetComponent<TextMeshProUGUI>().DOColor(Color.red, 0.2f);
-                foreach (var item in _listButtonsBackgrounds)
+                ChangeSelectedButton(-1);
+                ChangeColors();
+                ChangePositionButton();
+            }
+
+            if (Input.GetKeyDown(KeyCode.KeypadEnter))
+            {
+                DoActionButton();
+            }
+
+        }
+
+        void ChangePositionButton()
+        {
+            for (int i = 0; i < _listButtons.Count; i++)
+            {
+                if (i == _selectedButton)
                 {
-                    List<Image> images = new List<Image>(item.GetComponentsInChildren<Image>());
-                    images[0].DOColor(Color.grey.WithAlpha(1f), 0.2f);//.SetLoops(-1);
-                    images[0].DOFade(1, 0.2f);
-                    images[1].DOColor(Color.black.WithAlpha(1f), 0.2f);//.SetLoops(-1);
-                    images[1].DOFade(1, 0.2f);
+                    AnimationGoCenter(_listButtons[i], i);
+                    AnimationGoCenter(_listButtonsBackgrounds[i], i, 40);
+                    AnimationCenterCubes(_listButtonsBackgrounds[i]);
+                }
+                else
+                {
+                    AnimationGoLeft(_listButtons[i], i);
+                    AnimationGoLeft(_listButtonsBackgrounds[i], i, 20);
                 }
             }
-            else
+        }
+
+        void DoActionButton()
+        {
+            switch (_selectedButton)
             {
-                _listButtons[i].GetComponent<TextMeshProUGUI>().DOColor(Color.white, 0.2f);
-                foreach (var item in _listButtonsBackgrounds[i].GetComponentsInChildren<Image>())
-                {
-                    item.DOColor(Color.white.WithAlpha(0.1f), 0.2f);
-                }
-                
+                case 0:
+                    Play();
+                    break;
+                case 1:
+                    PlayEaster();
+                    break;
+                case 2:
+                    Options();
+                    break;
+                case 3:
+                    Quit();
+                    break;
             }
+        }
+
+        void Play()
+        {
+            _playButton.GetComponent<TextMeshProUGUI>().DOColor(Color.black, 0.2f);
+            SceneManager.LoadScene("MainScene");
+        }
+
+        void PlayEaster()
+        {
+            _playModButton.GetComponent<TextMeshProUGUI>().DOColor(Color.black, 0.2f);
+            SceneManager.LoadScene("DBDMode");
+
+        }
+
+        void Options()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+
+        void ChangeSelectedButton(int i)
+        {
+            _selectedButton += i;
+            if (_selectedButton < 0)
+            {
+                _selectedButton = _listButtons.Count - 1;
+            }
+            else if (_selectedButton >= _listButtons.Count)
+            {
+                _selectedButton = 0;
+            }
+        }
+
+        void ChangeColors()
+        {
+            for (int i = 0; i < _listButtons.Count; i++)
+            {
+                if (i == _selectedButton)
+                {
+                    _listButtons[i].GetComponent<TextMeshProUGUI>().DOColor(Color.red, 0.2f);
+                    foreach (var item in _listButtonsBackgrounds)
+                    {
+                        List<Image> images = new List<Image>(item.GetComponentsInChildren<Image>());
+                        images[0].DOColor(Color.grey.WithAlpha(1f), 0.2f);//.SetLoops(-1);
+                        images[0].DOFade(1, 0.2f);
+                        images[1].DOColor(Color.black.WithAlpha(1f), 0.2f);//.SetLoops(-1);
+                        images[1].DOFade(1, 0.2f);
+                    }
+                }
+                else
+                {
+                    _listButtons[i].GetComponent<TextMeshProUGUI>().DOColor(Color.white, 0.2f);
+                    foreach (var item in _listButtonsBackgrounds[i].GetComponentsInChildren<Image>())
+                    {
+                        item.DOColor(Color.white.WithAlpha(0.1f), 0.2f);
+                    }
+
+                }
+            }
+        }
+
+
+        public void ChangeScene()
+        {
+            SceneManager.LoadScene("MainScene");
         }
     }
 
-
-    public void ChangeScene()
-    {
-        SceneManager.LoadScene("MainScene");
-    }
-}
