b2810ae521d0e3422f4ba2d953cc6bde8dcc48f3
Cookie <88719371+Cookiezikse@users.noreply.github.com>
2024-09-13 07:53:14 +0200

    [ADD] ColorExtension [FIX] Build now works

--- Assets/DoorGenerator.cs
+++ Assets/DoorGenerator.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
+
 public class DoorGenerator : MonoBehaviour
 {
+
+
 
     SpriteRenderer _spriteRenderer;
     CircleCollider2D _circleCollider;
